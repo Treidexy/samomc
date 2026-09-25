52aed362b65808cec369f9639453624a05dfa159
Treidex <treidexy@gmail.com>
2026-09-25 08:18:32 -0700

    Update flake.nix

--- flake.nix
+++ flake.nix
@@ -4,6 +4,9 @@
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs/nixos-26.05";
     nix-minecraft.url = "github:Infinidoge/nix-minecraft";
+
+    sops-nix.url = "github:Mic92/sops-nix";
+    sops-nix.inputs.nixpkgs.follows = "nixpkgs";
   };
 
   outputs = { self, nixpkgs, nix-minecraft }: {
@@ -11,6 +14,7 @@
       imports = [
         nix-minecraft.nixosModules.minecraft-servers
         ./mcserver.nix
+        sops-nix.nixosModules.sops
       ];
 
       nixpkgs.overlays = [ nix-minecraft.overlay ];
