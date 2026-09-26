bf393208eee3ce5124fda032e95e090756ca0fbd
Treidex <treidexy@gmail.com>
2026-09-25 21:33:30 -0700

    Remove sops-nix input from flake.nix
    Removed sops-nix input from flake.nix.

--- flake.nix
+++ flake.nix
@@ -4,9 +4,6 @@
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs/nixos-26.05";
     nix-minecraft.url = "github:Infinidoge/nix-minecraft";
-
-    sops-nix.url = "github:Mic92/sops-nix";
-    sops-nix.inputs.nixpkgs.follows = "nixpkgs";
   };
 
   outputs = { self, nixpkgs, nix-minecraft }: {
